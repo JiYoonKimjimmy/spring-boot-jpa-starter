de95e321f402da6daa8750665f54986d1e4b7e93
jimmyberg <jimmyberg.kim@gmail.com>
2022-09-22 16:51:22 +0900

    embeddable 실습 추가

--- src/main/kotlin/me/jimmyberg/jpa/entity/Member.kt
+++ src/main/kotlin/me/jimmyberg/jpa/entity/Member.kt
@@ -1,8 +1,6 @@
 package me.jimmyberg.jpa.entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity(name = "MEMBERS")
 class Member(
@@ -10,5 +8,22 @@ class Member(
     @GeneratedValue
     val id: Long? = null,
     val name: String,
-    var age: Long
+    var age: Long,
+    @Embedded
+    val address: Address? = null,
+    @Embedded
+    @AttributeOverrides(
+        AttributeOverride(name = "zipCode", column = Column(name = "HOME_ZIP_CODE")),
+        AttributeOverride(name = "baseAddress", column = Column(name = "HOME_BASE_ADDRESS")),
+        AttributeOverride(name = "detailAddress", column = Column(name = "HOME_DETAIL_ADDRESS"))
+    )
+    val homeAddress: Address? = null
+)
+
+@Embeddable
+data class Address(
+    @Column(name = "ZIP_CODE")
+    val zipCode: String,
+    val baseAddress: String,
+    val detailAddress: String
 )
